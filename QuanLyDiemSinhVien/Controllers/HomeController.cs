13d904de2ec74b396a1286e91deba9b85e5f00b8
hieuhuc <65611182+hieuhuc@users.noreply.github.com>
2021-05-21 12:32:06 +0700

    first commit

---
+++ QuanLyDiemSinhVien/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyDiemSinhVien.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Nhóm 3: Trần Minh Quân, Trần Trung Hiếu";
+
+            return View();
+        }
+        
+        public ActionResult Contact()   
+        {
+            ViewBag.Message = "Quản lý sinh viên đại học";
+
+            return View();
+        }
+    }
+}
